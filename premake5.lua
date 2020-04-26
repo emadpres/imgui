a470dc818690d5f0b07bba269b7315c2f63c5b19
Emad Aghajani <emadpres@gmail.com>
2020-04-26 02:43:11 +0200

    Premake5 file

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+-- Source: https://github.com/TheCherno/imgui/blob/ee5ce4bccae2b8474a3e57c0c2a1eb2807f2de8b/premake5.lua
+-- This script is supposed to be included by TheCherno's Game Engine project.
+project "ImGui"
+    kind "StaticLib"
+	language "C++"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
